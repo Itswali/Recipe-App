2433ca8d1a0e143052f6835092b8570adcd9c593
Balikuddembe <balixemma@gmail.com>
2023-06-28 02:12:02 +0300

    Create public_recipes controller and index action

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,7 @@
+class PublicRecipesController < ApplicationController
+    def index
+      @users = User.all
+      @public_recipes = Recipe.includes(%i[recipe_foods user]).where(public: true).order(created_at: :desc)
+    end
+  end
+  
