22cf38a38271a4f435a2f38138d5a284ce8f8262
Itswali <luckywali0986@gmail.com>
2023-06-27 14:50:43 +0500

    added all the migrations files accordingn to the migrations.

---
+++ db/migrate/20230627094340_create_shopping_lists.rb
@@ -0,0 +1,9 @@
+class CreateShoppingLists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shopping_lists do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
