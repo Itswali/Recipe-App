edc746257e775b7442a5786c2b536976a7bcd46c
Balikuddembe <balixemma@gmail.com>
2023-06-27 17:29:29 +0300

    Fix rubocop linters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ApplicationRecord
-    has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id'
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id'
 
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-    validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
