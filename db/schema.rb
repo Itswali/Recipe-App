a818d7c82fefca48051f80194e06972997fe7d6a
Itswali <luckywali0986@gmail.com>
2023-06-28 16:49:02 +0500

    add back to main button

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema[7.0].define(version: 2023_06_27_154224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_154224) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_093653) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
