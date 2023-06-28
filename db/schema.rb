53dc249a1614518e83b4a55d65730e5f638f7b30
Itswali <luckywali0986@gmail.com>
2023-06-28 21:53:58 +0500

    fix some minor issue about undefined things

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_001133) do
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_154224) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_212621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
-    t.string "measurement_unit"
-    t.decimal "price"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.string "measuring_unit"
     t.decimal "price", precision: 8, scale: 2
     t.integer "quantity"
@@ -60,8 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_154224) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
     t.string "name"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
