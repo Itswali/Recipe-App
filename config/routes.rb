f8ab916debeb9ae9d8114d2bd7cc56382101ee53
Itswali <luckywali0986@gmail.com>
2023-06-29 21:16:34 +0500

    add route links to shooping list and public recipe

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     post 'toggle', on: :member
   end
+  resources :shopping_list, only: [:index]
+  resources :public_recipes, only: [:index]
 
   # Your other routes go here
 end
