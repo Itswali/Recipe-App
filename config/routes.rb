12a57700592f2a926258bd0304025c140c90a62b
Itswali <luckywali0986@gmail.com>
2023-06-29 20:55:04 +0500

    add route for deleting a food

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :foods, only: [:index, :new, :create]
+  resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     post 'toggle', on: :member
   end
