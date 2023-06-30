ea96bc67d7047272396e965f348a5713b2be9473
Balikuddembe <balixemma@gmail.com>
2023-06-30 13:24:59 +0300

    Add unit tests for the recipe model

---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,17 @@
+# spec/models/recipe_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+    let(:user) { User.create!(name: 'recipe', email: 'balix@gmail.com', password: 'password') }
+    let(:recipe) { Recipe.create(name: 'Recipe 1', description: 'Recipe description', user: user) }
+  
+    it 'should have name recipe' do
+      expect(recipe.name).to eql('Recipe 1')
+    end
+  
+    it 'should have description' do
+      expect(recipe.description).to eql('Recipe description')
+    end
+  end
+  
