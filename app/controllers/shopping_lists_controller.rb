b3864cf4b8b856940d7a6f3ef8319193c6e5f7a9
Itswali <luckywali0986@gmail.com>
2023-07-01 19:26:36 +0500

    fix the shooping list error

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -5,9 +5,10 @@ class ShoppingListsController < ApplicationController
     @total_foods = 0
     @total_price = 0
     @recipe_foods = []
+
     @user.recipes.each do |recipe|
       recipe.recipe_foods.each do |recipe_food|
-        @total_foods += 1
+        @total_foods += recipe_food.quantity
         @total_price += recipe_food.food.price * recipe_food.quantity
         @recipe_foods << recipe_food
       end
